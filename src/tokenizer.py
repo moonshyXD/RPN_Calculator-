7e0aa976030dcfed2caeea17becf27e89b259b54
moonshy <vladbobkb10@gmail.com>
2025-10-03 20:52:36 +0300

    processed unary minus commit

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -2,6 +2,25 @@ from typing import List
 from .numbers import to_number
 
 
+def handle_unary_minus(token: str) -> str:
+    """
+    Process a token with one or multiple '~' prefixes representing unary minus.
+    :param token: The token starting with '~'.
+    :return: String result with precessed unary minus.
+    """
+    count_minus = 0
+    result: str
+    for i in range(len(token)):
+        if token[i] == "~":
+            count_minus += 1
+        else:
+            result = str(to_number(token[i:]))
+            if count_minus % 2 != 0:
+                result = "-" + result
+
+    return result
+
+
 def tokenize(expression: str) -> List[str]:
     """
     Tokenize an RPN expression line into a list of tokens.
@@ -13,15 +32,16 @@ def tokenize(expression: str) -> List[str]:
     """
     raw_tokens = expression.strip().split()
     tokens = []
-    for p in raw_tokens:
-        if p == '' or p is None:
+    for token in raw_tokens:
+        if token == '' or token is None:
             continue
-        if p in ("(", ")"):
-            tokens.append(p)
-        if p[0] == "~":
-            tokens.append(str(-to_number(p[1:])))
-        elif p[0] == "$":
-            tokens.append(p[1:])
+        if token in ("(", ")"):
+            tokens.append(token)
+        if token[0] == "~":
+            to_append = handle_unary_minus(token)
+            tokens.append(to_append)
+        elif token[0] == "$":
+            tokens.append(token[1:])
         else:
-            tokens.append(p)
+            tokens.append(token)
     return tokens
